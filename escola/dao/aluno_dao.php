5604e0ac36379c9a02da0d6f60c9dc0eae56b057
Matheus Dutra <74274788+matheusdutraa@users.noreply.github.com>
2022-10-11 06:36:22 -0300

    Update aluno_dao.php

--- escola/dao/aluno_dao.php
+++ escola/dao/aluno_dao.php
@@ -21,10 +21,11 @@ include_once 'model/curso.php';
 
 class AlunoDAO {
     public function list() {
-        $sql = "SELECT a.id, a.nome, a.email, a.curso_id, c.nome AS nome_curso FROM alunos a INNER JOIN cursos c ON a.curso_id = c.id";
+        $sql = "SELECT a.idAluno, a.nomeAluno, a.idade, a.estrangeiro, c.nome AS nome_curso FROM alunos a INNER JOIN cursos c ON a.curso_id = c.id";
         $result = mysql_query($sql);
         $lista = array();
         while($reg = mysql_fetch_assoc($result)) {
+            $curso = new Curso($reg["curso_id"], $reg["nome_curso"]);
             $aluno = new Aluno($reg["idAluno"], $reg["nomeAluno "], $reg["idade "], $reg["estrangeiro "], $curso);
             $lista[] = $aluno;
         }
